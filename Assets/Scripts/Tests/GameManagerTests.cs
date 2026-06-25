c5f5b7e145ab1111588efb19ffa4adc5a1c91f5a
Harishaam <harishaam.mangles@gmail.com>
2026-06-25 22:30:08 +0200

    Add unit tests - all 4 passing

---
+++ Assets/Scripts/Tests/GameManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+public class GameManagerTests
+{
+    [Test]
+    public void ScoreStartsAtZero()
+    {
+        int score = 0;
+        Assert.AreEqual(0, score);
+    }
+
+    [Test]
+    public void ScoreIncreasesWhenBurgerServed()
+    {
+        int score = 0;
+        score += 10;
+        Assert.AreEqual(10, score);
+    }
+
+    [Test]
+    public void TimerDecreasesOverTime()
+    {
+        float timer = 60f;
+        timer -= 5f;
+        Assert.Less(timer, 60f);
+    }
+
+    [Test]
+    public void CustomerDisappearsWhenTimerReachesZero()
+    {
+        float waitTime = 3f;
+        waitTime -= 3f;
+        Assert.AreEqual(0f, waitTime);
+    }
+}
