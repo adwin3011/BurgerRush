863d3683c1d4e1fdf7393ef297124aee10d18595
Harishaam <harishaam.mangles@gmail.com>
2026-06-25 21:15:01 +0200

    Add CameraFollow script - camera tracks chef

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,7 +1,13 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
 {
-    public class CameraFollow
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 10, -7);
+
+    void LateUpdate()
     {
-        
+        transform.position = target.position + offset;
+        transform.LookAt(target);
     }
 }
