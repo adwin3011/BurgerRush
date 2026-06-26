eeff35976963fece80480423d5522dcd3c7b6210
Harishaam <harishaam.mangles@gmail.com>
2026-06-27 01:21:25 +0200

    Add Score and Timer UI, top-down camera, materials complete

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,11 +3,11 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(0, 10, -7);
+    public Vector3 offset = new Vector3(0, 20, 0);
 
     void LateUpdate()
     {
         transform.position = target.position + offset;
-        transform.LookAt(target);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+    
+    private int score = 0;
+    private float timeLeft = 60f;
+    private bool gameOver = false;
+
+    void Update()
+    {
+        if (!gameOver)
+        {
+            timeLeft -= Time.deltaTime;
+            timerText.text = "Time: " + Mathf.CeilToInt(timeLeft);
+
+            if (timeLeft <= 0)
+            {
+                timeLeft = 0;
+                gameOver = true;
+                timerText.text = "Time: 0";
+            }
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+    }
+}
